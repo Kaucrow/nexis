862f7df3112a9a44dfc1727b61e4ce7a2a30a1fd
kaucrow <kaucrow@protonmail.com>
2024-09-21 16:10:05 -0400

    fix: Removed frontend dark mode flicker on page mount

--- ActixLogin/frontend/src/lib/stores/common.store.ts
+++ ActixLogin/frontend/src/lib/stores/common.store.ts
@@ -2,3 +2,4 @@ import { writable } from 'svelte/store';
 import type { CustomError } from '$lib/utils/types';
 
 export const errStore = writable<CustomError[]>([]);
+export const darkModeStore = writable<boolean>(false);
--- ActixLogin/frontend/src/routes/+layout.ts
+++ ActixLogin/frontend/src/routes/+layout.ts
@@ -2,10 +2,6 @@ import { getNum } from '$lib/num_ops/num_ops';
 import type { Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ fetch }) => {
-    const { initialValue, errors } = await getNum(fetch);
-    return {
-        initialValue,
-        errors,
-        fetch,
-    };
+    return { fetch };
 }
+
