d3008c384368f90c438ca50a0877c27861160f05
CornyBread <davidesber7@gmail.com>
2024-10-21 22:16:31 -0400

    feat: Added PostgreSQL tables for Admin, Schedule, Jobs, EmployeeJob, Stores, and StoreJobs

--- db-model/postgres/NexisTables.sql
+++ db-model/postgres/NexisTables.sql
@@ -2,27 +2,63 @@ CREATE TABLE Users (
     uuid SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Clients (
-    userUuid SERIAL PRIMARY KEY,
-    FOREIGN KEY (userUuid) REFERENCES Users(uuid),
+    clientUuid SERIAL PRIMARY KEY,
+    FOREIGN KEY (clientUuid) REFERENCES Users(uuid),
     phone VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     gender VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Employees (
-    userUuid SERIAL PRIMARY KEY,
-    FOREIGN KEY (userUuid) REFERENCES Users(uuid),
+    employeeUuid SERIAL PRIMARY KEY,
+    FOREIGN KEY (employeeUuid) REFERENCES Users(uuid),
     phone VARCHAR(255) NOT NULL,
     age INT NOT NULL
 );
 
+CREATE TABLE Admin (
+    adminUuid SERIAL PRIMARY KEY,
+    FOREIGN KEY (adminUuid) REFERENCES Users(uuid)
+);
 
+CREATE TABLE Schedule (
+    uuid SERIAL PRIMARY KEY,
+    employeeUuid SERIAL,
+    FOREIGN KEY (employeeUuid) REFERENCES Employees(employeeUuid),
+    enterDate DATE NOT NULL,
+    exitDate DATE NOT NULL
+);
 
+CREATE TABLE Jobs (
+    uuid SERIAL PRIMARY KEY,
+    jobName VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE EmployeeJob (
+    scheduleUuid SERIAL PRIMARY KEY,
+    FOREIGN KEY (scheduleUuid) REFERENCES Schedule(uuid),
+    jobUuid SERIAL,
+    FOREIGN KEY (jobUuid) REFERENCES Jobs(uuid)
+);
 
+CREATE TABLE Stores (
+    uuid SERIAL PRIMARY KEY,
+    storeName VARCHAR(255) NOT NULL,
+    storeNum INT NOT NULL,
+    storeFloor INT NOT NULL,
+    storeLenght INT NOT NULL,
+    storeWidth INT NOT NULL,
+    storeHeight INT NOT NULL
+);
 
+CREATE TABLE StoreJobs (
+    storeUuid SERIAL PRIMARY KEY,
+    FOREIGN KEY (storeUuid) REFERENCES Stores(uuid),
+    jobUuid SERIAL,
+    FOREIGN KEY (jobUuid) REFERENCES Jobs(uuid)
+);
